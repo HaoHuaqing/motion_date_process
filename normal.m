8c02a3dd63041467bbe8ef775b58d1253e4be0d9
HaoHuaqing <534390598@qq.com>
2017-08-21 10:56:27 +0800

    make plot automation

--- normal.m
+++ normal.m
@@ -1,86 +1,88 @@
 clc
 clear
 
-i=1;
-num='1.5';
-subplot(2,3,6);
-str=strcat('disturbance=',num);
-hhq_str=strcat('hhq_',num,'.csv');
-lxd_str=strcat('lxd_',num,'.csv');
-xfl_str=strcat('xfl_',num,'.csv');
-hzx_str=strcat('hzx_',num,'.csv');
+model_chose = [0;0;0;1;1;1];
+num = ['0.0';'0.3';'0.6';'0.9';'1.2';'1.5'];
+for n = 1:6
+    subplot(2,3,n);
+    str=strcat('disturbance=',num(n,:));
+    hhq_str=strcat('hhq_',num(n,:),'.csv');
+    lxd_str=strcat('lxd_',num(n,:),'.csv');
+    xfl_str=strcat('xfl_',num(n,:),'.csv');
+    hzx_str=strcat('hzx_',num(n,:),'.csv');
 
-hhq = csvread(hhq_str, 2, 0);
-stickposition=hhq(:,1);
-actualposition=hhq(:,2);
-roadpositation=hhq(:,3);
-a1=actualposition-roadpositation;
+    hhq = csvread(hhq_str, 2, 0);
+    stickposition=hhq(:,1);
+    actualposition=hhq(:,2);
+    roadpositation=hhq(:,3);
+    a1=actualposition-roadpositation;
 
-lxd = csvread(lxd_str, 2, 0);
-stickposition=lxd(:,1);
-actualposition=lxd(:,2);
-roadpositation=lxd(:,3);
-a2=actualposition-roadpositation;
+    lxd = csvread(lxd_str, 2, 0);
+    stickposition=lxd(:,1);
+    actualposition=lxd(:,2);
+    roadpositation=lxd(:,3);
+    a2=actualposition-roadpositation;
 
-xfl = csvread(xfl_str, 2, 0);
-stickposition=xfl(:,1);
-actualposition=xfl(:,2);
-roadpositation=xfl(:,3);
-a3=actualposition-roadpositation;
+    xfl = csvread(xfl_str, 2, 0);
+    stickposition=xfl(:,1);
+    actualposition=xfl(:,2);
+    roadpositation=xfl(:,3);
+    a3=actualposition-roadpositation;
 
-hzx = csvread(hzx_str, 2, 0);
-stickposition=hzx(:,1);
-actualposition=hzx(:,2);
-roadpositation=hzx(:,3);
-a4=actualposition-roadpositation;
+    hzx = csvread(hzx_str, 2, 0);
+    stickposition=hzx(:,1);
+    actualposition=hzx(:,2);
+    roadpositation=hzx(:,3);
+    a4=actualposition-roadpositation;
 
-data=[a1;a2;a3;a4];
-if i>0
-    [f,xc]=ecdf(data);
-    ecdfhist(f,xc,100);
-    h = findobj(gca,'Type','patch');
-    h.FaceColor = [.5 .5 .5];
-    h.EdgeColor = 'w';
-    ylim([0 0.5])
-    t = title(str);
-    set(t, 'FontSize', 20);
-    set(gca,'FontSize',16)
-    x=-10:0.05:10;
-    y=normpdf(x,mean(data),std(data));
-    hold on 
-    plot(x,y,'k','LineWidth',2);
-    [h,p,stats]=chi2gof(data,'nbins',20)
+    data=[a1;a2;a3;a4];
+    if model_chose(n)>0
+        [f,xc]=ecdf(data);
+        ecdfhist(f,xc,100);
+        h = findobj(gca,'Type','patch');
+        h.FaceColor = [.5 .5 .5];
+        h.EdgeColor = 'w';
+        ylim([0 0.5])
+        t = title(str);
+        set(t, 'FontSize', 20);
+        set(gca,'FontSize',16)
+        x=-10:0.05:10;
+        y=normpdf(x,mean(data),std(data));
+        hold on 
+        plot(x,y,'k','LineWidth',2);
+        [h,p,stats]=chi2gof(data,'nbins',20)
+    end
+
+    if model_chose(n)==0
+        [f,xc]=ecdf(data);
+        ecdfhist(f,xc,100);
+        h = findobj(gca,'Type','patch');
+        h.FaceColor = [.5 .5 .5];
+        h.EdgeColor = 'w';
+        pdf_normmixture = @(data,p,mu1,mu2,sigma1,sigma2)p*normpdf(data,mu1,sigma1) + (1-p)*normpdf(data,mu2,sigma2);
+        pStart = .5;
+        muStart = quantile(data,[0.25 0.75])
+        sigmaStart = sqrt(var(data) - .25*diff(muStart).^2)
+        start = [pStart muStart sigmaStart sigmaStart];
+        lb = [0 -Inf -Inf 0 0];
+        ub = [1 Inf Inf Inf Inf];
+        paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start, 'lower',lb, 'upper',ub)
+        statset('mlecustom')
+        options = statset('MaxIter',100000, 'MaxFunEvals',200000);
+        paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start,'lower',lb, 'upper',ub, 'options',options)
+        x=-10:0.05:10;
+        y = pdf_normmixture(x,paramEsts(1),paramEsts(2),paramEsts(3),paramEsts(4),paramEsts(5));
+        z1 = paramEsts(1)*normpdf(x,paramEsts(2),paramEsts(4));
+        z2 = (1-paramEsts(1))*normpdf(x,paramEsts(3),paramEsts(5));
+        hold on;
+        plot(x,z1,'--b','LineWidth',2)
+        plot(x,z2,'--b','LineWidth',2)
+        plot(x,y,'k','LineWidth',2)
+        ylim([0 0.5])
+        t = title(str);
+        set(t, 'FontSize', 20);
+        set(gca,'FontSize',16)
+        [h,p,stats]=chi2gof(data,'nbins',20)
+    end
+    %disp(paramEsts(1)*(1-paramEsts(1)))
 end
-
-if i==0
-    [f,xc]=ecdf(data);
-    ecdfhist(f,xc,100);
-    h = findobj(gca,'Type','patch');
-    h.FaceColor = [.5 .5 .5];
-    h.EdgeColor = 'w';
-    pdf_normmixture = @(data,p,mu1,mu2,sigma1,sigma2)p*normpdf(data,mu1,sigma1) + (1-p)*normpdf(data,mu2,sigma2);
-    pStart = .5;
-    muStart = quantile(data,[0.25 0.75])
-    sigmaStart = sqrt(var(data) - .25*diff(muStart).^2)
-    start = [pStart muStart sigmaStart sigmaStart];
-    lb = [0 -Inf -Inf 0 0];
-    ub = [1 Inf Inf Inf Inf];
-    paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start, 'lower',lb, 'upper',ub)
-    statset('mlecustom')
-    options = statset('MaxIter',100000, 'MaxFunEvals',200000);
-    paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start,'lower',lb, 'upper',ub, 'options',options)
-    x=-10:0.05:10;
-    y = pdf_normmixture(x,paramEsts(1),paramEsts(2),paramEsts(3),paramEsts(4),paramEsts(5));
-    z1 = paramEsts(1)*normpdf(x,paramEsts(2),paramEsts(4));
-    z2 = (1-paramEsts(1))*normpdf(x,paramEsts(3),paramEsts(5));
-    hold on;
-    plot(x,z1,'--b','LineWidth',2)
-    plot(x,z2,'--b','LineWidth',2)
-    plot(x,y,'k','LineWidth',2)
-    ylim([0 0.5])
-    t = title(str);
-    set(t, 'FontSize', 20);
-    set(gca,'FontSize',16)
-    [h,p,stats]=chi2gof(data,'nbins',20)
-end
-%disp(paramEsts(1)*(1-paramEsts(1)))
--- patient.m
+++ patient.m
@@ -1,94 +1,96 @@
 clc
 clear
 
-i=0;
-num='0.0';
-subplot(2,3,6);
-str=strcat('disturbance=',num);
-gyq_str=strcat('gyq_',num,'.csv');
-wlh_str=strcat('wlh_',num,'.csv');
-yyp_str=strcat('yyp_',num,'.csv');
-zrj_str=strcat('zrj_',num,'.csv');
-chj_str=strcat('chj_',num,'.csv');
+model_chose = [0;0;0;1;1;1];
+num = ['0.0';'0.3';'0.6';'0.9';'1.2';'1.5'];
+for n = 1:6
+    subplot(2,3,n);
+    str=strcat('disturbance=',num(n,:));
+    gyq_str=strcat('gyq_',num(n,:),'.csv');
+    wlh_str=strcat('wlh_',num(n,:),'.csv');
+    yyp_str=strcat('yyp_',num(n,:),'.csv');
+    zrj_str=strcat('zrj_',num(n,:),'.csv');
+    chj_str=strcat('chj_',num(n,:),'.csv');
 
-gyq = csvread(gyq_str, 2, 0);
-stickposition=gyq(:,1);
-actualposition=gyq(:,2);
-roadpositation=gyq(:,3);
-a1=actualposition-roadpositation;
+    gyq = csvread(gyq_str, 2, 0);
+    stickposition=gyq(:,1);
+    actualposition=gyq(:,2);
+    roadpositation=gyq(:,3);
+    a1=actualposition-roadpositation;
 
-wlh = csvread(wlh_str, 2, 0);
-stickposition=wlh(:,1);
-actualposition=wlh(:,2);
-roadpositation=wlh(:,3);
-a2=actualposition-roadpositation;
+    wlh = csvread(wlh_str, 2, 0);
+    stickposition=wlh(:,1);
+    actualposition=wlh(:,2);
+    roadpositation=wlh(:,3);
+    a2=actualposition-roadpositation;
 
-yyp = csvread(yyp_str, 2, 0);
-stickposition=yyp(:,1);
-actualposition=yyp(:,2);
-roadpositation=yyp(:,3);
-a3=actualposition-roadpositation;
+    yyp = csvread(yyp_str, 2, 0);
+    stickposition=yyp(:,1);
+    actualposition=yyp(:,2);
+    roadpositation=yyp(:,3);
+    a3=actualposition-roadpositation;
 
-zrj = csvread(zrj_str, 2, 0);
-stickposition=zrj(:,1);
-actualposition=zrj(:,2);
-roadpositation=zrj(:,3);
-a4=actualposition-roadpositation;
+    zrj = csvread(zrj_str, 2, 0);
+    stickposition=zrj(:,1);
+    actualposition=zrj(:,2);
+    roadpositation=zrj(:,3);
+    a4=actualposition-roadpositation;
 
-chj = csvread(chj_str, 2, 0);
-stickposition=chj(:,1);
-actualposition=chj(:,2);
-roadpositation=chj(:,3);
-a5=actualposition-roadpositation;
+    chj = csvread(chj_str, 2, 0);
+    stickposition=chj(:,1);
+    actualposition=chj(:,2);
+    roadpositation=chj(:,3);
+    a5=actualposition-roadpositation;
 
-data=[a1;a2;a3;a4;a5];
+    data=[a1;a2;a3;a4;a5];
 
-if i>0
-    [f,xc]=ecdf(data);
-    ecdfhist(f,xc,100);
-    h = findobj(gca,'Type','patch');
-    h.FaceColor = [.5 .5 .5];
-    h.EdgeColor = 'w';
-    ylim([0 0.5])
-    t = title(str);
-    set(t, 'FontSize', 20);
-    set(gca,'FontSize',16)
-    x=-10:0.05:10;
-    y=normpdf(x,mean(data),std(data));
-    hold on 
-    plot(x,y,'k','LineWidth',2);
-    [h,p,stats]=chi2gof(data,'nbins',20)
+    if model_chose(n)>0
+        [f,xc]=ecdf(data);
+        ecdfhist(f,xc,100);
+        h = findobj(gca,'Type','patch');
+        h.FaceColor = [.5 .5 .5];
+        h.EdgeColor = 'w';
+        ylim([0 0.5])
+        t = title(str);
+        set(t, 'FontSize', 20);
+        set(gca,'FontSize',16)
+        x=-10:0.05:10;
+        y=normpdf(x,mean(data),std(data));
+        hold on 
+        plot(x,y,'k','LineWidth',2);
+        [h,p,stats]=chi2gof(data,'nbins',20)
+    end
+
+    if model_chose(n)==0
+        [f,xc]=ecdf(data);
+        ecdfhist(f,xc,100);
+        h = findobj(gca,'Type','patch');
+        h.FaceColor = [.5 .5 .5];
+        h.EdgeColor = 'w';
+        pdf_normmixture = @(data,p,mu1,mu2,sigma1,sigma2)p*normpdf(data,mu1,sigma1) + (1-p)*normpdf(data,mu2,sigma2);
+        pStart = .5;
+        muStart = quantile(data,[0.25 0.75])
+        sigmaStart = sqrt(var(data) - .25*diff(muStart).^2)
+        start = [pStart muStart sigmaStart sigmaStart];
+        lb = [0 -Inf -Inf 0 0];
+        ub = [1 Inf Inf Inf Inf];
+        paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start, 'lower',lb, 'upper',ub)
+        statset('mlecustom')
+        options = statset('MaxIter',100000, 'MaxFunEvals',200000);
+        paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start,'lower',lb, 'upper',ub, 'options',options)
+        x=-10:0.05:10;
+        y = pdf_normmixture(x,paramEsts(1),paramEsts(2),paramEsts(3),paramEsts(4),paramEsts(5));
+        z1 = paramEsts(1)*normpdf(x,paramEsts(2),paramEsts(4));
+        z2 = (1-paramEsts(1))*normpdf(x,paramEsts(3),paramEsts(5));
+        hold on;
+        plot(x,z1,'--b','LineWidth',2)
+        plot(x,z2,'--b','LineWidth',2)
+        plot(x,y,'k','LineWidth',2)
+        ylim([0 0.5])
+        t = title(str);
+        set(t, 'FontSize', 20);
+        set(gca,'FontSize',16)
+        [h,p,stats]=chi2gof(data,'nbins',20)
+    end
+    %disp(paramEsts(1)*(1-paramEsts(1)))
 end
-
-if i==0
-    [f,xc]=ecdf(data);
-    ecdfhist(f,xc,100);
-    h = findobj(gca,'Type','patch');
-    h.FaceColor = [.5 .5 .5];
-    h.EdgeColor = 'w';
-    pdf_normmixture = @(data,p,mu1,mu2,sigma1,sigma2)p*normpdf(data,mu1,sigma1) + (1-p)*normpdf(data,mu2,sigma2);
-    pStart = .5;
-    muStart = quantile(data,[0.25 0.75])
-    sigmaStart = sqrt(var(data) - .25*diff(muStart).^2)
-    start = [pStart muStart sigmaStart sigmaStart];
-    lb = [0 -Inf -Inf 0 0];
-    ub = [1 Inf Inf Inf Inf];
-    paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start, 'lower',lb, 'upper',ub)
-    statset('mlecustom')
-    options = statset('MaxIter',100000, 'MaxFunEvals',200000);
-    paramEsts = mle(data, 'pdf',pdf_normmixture, 'start',start,'lower',lb, 'upper',ub, 'options',options)
-    x=-10:0.05:10;
-    y = pdf_normmixture(x,paramEsts(1),paramEsts(2),paramEsts(3),paramEsts(4),paramEsts(5));
-    z1 = paramEsts(1)*normpdf(x,paramEsts(2),paramEsts(4));
-    z2 = (1-paramEsts(1))*normpdf(x,paramEsts(3),paramEsts(5));
-    hold on;
-    plot(x,z1,'--b','LineWidth',2)
-    plot(x,z2,'--b','LineWidth',2)
-    plot(x,y,'k','LineWidth',2)
-    ylim([0 0.5])
-    t = title(str);
-    set(t, 'FontSize', 20);
-    set(gca,'FontSize',16)
-    [h,p,stats]=chi2gof(data,'nbins',20)
-end
-%disp(paramEsts(1)*(1-paramEsts(1)))
